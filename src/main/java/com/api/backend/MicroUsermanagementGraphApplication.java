4f42b7842e90fbe9f4f0331cea1621da1bc30a4a
Xxander16 <anthonysistemas159@gmail.com>
2024-12-03 12:31:46 -0500

    first commit

---
+++ src/main/java/com/api/backend/MicroUsermanagementGraphApplication.java
@@ -0,0 +1,13 @@
+package com.api.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroUsermanagementGraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroUsermanagementGraphApplication.class, args);
+	}
+
+}
